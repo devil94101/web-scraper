3e1a09f83b95555e75683ee4c74f9513ccb022f5
devil94101 <36105759+devil94101@users.noreply.github.com>
2020-07-21 19:11:07 +0530

    Update Blogs.jsx

--- src/Blogs/Blogs.jsx
+++ src/Blogs/Blogs.jsx
@@ -26,7 +26,8 @@ function blogs({submit,name,paginate,cng,post,postPerPage,searched,result,blogPa
               </div>
               <div className="col-sm-3 offset-md-1">
                <div className="row">
-                  <h1>Search History</h1>
+                  <h1>Knock knock tera Baap AAya</h1>
+                   <h4>Deepak Paliwal</h4>
                </div>
               </div>
             </div>
